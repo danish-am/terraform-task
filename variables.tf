e927dea159fe62fa0998610fdc2d99cd01f9e7d5
danish-am <ahmeddanish1729@gmail.com>
2025-08-20 21:15:03 -0400

    Add public and private subnets across availability zones

--- variables.tf
+++ variables.tf
@@ -6,3 +6,17 @@ variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+}
