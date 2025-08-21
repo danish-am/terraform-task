4989a5c4a758b430ecc231be6b56f824a07768f5
danish-am <ahmeddanish1729@gmail.com>
2025-08-20 21:48:56 -0400

    Provision EC2 instance for nginx web server

---
+++ instance.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux_2.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y nginx
+    systemctl start nginx
+    systemctl enable nginx
+  EOF
+
+  tags = {
+    Name = "nginx-web-server"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -20,3 +20,7 @@ variable "private_subnet_cidrs" {
   type    = list(string)
   default = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
 }
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
