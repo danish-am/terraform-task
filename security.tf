55c8904fbc7f3e68b530da4cc0674cb202730d02
danish-am <ahmeddanish1729@gmail.com>
2025-08-20 21:31:16 -0400

    Create security group for web server HTTP access

---
+++ security.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "web" {
+  name        = "web-security-group"
+  description = "Security group for web server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
